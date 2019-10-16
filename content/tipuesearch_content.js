831a17e9e88164a40f5f2f0ef87beedcb9dfb6ce
40823144 <40823144@gm.nfu.edu.tw>
2019-10-17 01:02:53 +0800

    initial add

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'image', 'text': 'About \n https://40823144.github.io/cp2019 \xa0\xa0 \n https://dartpad.dev/?id=6954ff215502203a0fd0e9961ba9cc03 \n', 'tags': '', 'url': 'image.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'About \n https://40823144.github.io/cp2019 \xa0\xa0 \n https://dartpad.dev/?id=6954ff215502203a0fd0e9961ba9cc03 \n', 'tags': '', 'url': 'About.html'}]};
