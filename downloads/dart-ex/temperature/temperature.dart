6dfe330d0ac4ac619835086b0f263fe70656ed79
40823144 <40823144@gm.nfu.edu.tw>
2019-12-12 00:59:03 +0800

    hgjhk

---
+++ downloads/dart-ex/temperature/temperature.dart
@@ -0,0 +1,19 @@
+
+main() {
+  List temp = ["20C", "30C", "50F", "40C", "23F"];
+  for (var i in temp) {
+  
+    if (i[2] == "C") {
+    var a = (i[0]+i[1]);
+     var c = int.parse (a);
+    double f =c*9/5+32;
+    print ("攝氏 $c 度=華氏 $f 度");
+    }
+    else{
+      var a = (i[0]+i[1]);
+     var f = int.parse (a);
+    double c =(f-32)*5/9;
+    print ("華氏 $f 度=攝氏 $c 度 ");
+    }
+  } 
+} 
